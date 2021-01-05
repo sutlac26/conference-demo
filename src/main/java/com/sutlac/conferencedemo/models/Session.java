6c30b10157859e018d7b3ea1b686156805b6257e
sutlac26 <54089898+sutlac26@users.noreply.github.com>
2021-01-04 22:30:50 -0500

    Creating Entity

--- src/main/java/com/sutlac/conferencedemo/models/Session.java
+++ src/main/java/com/sutlac/conferencedemo/models/Session.java
@@ -1,9 +1,7 @@
 package com.sutlac.conferencedemo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity(name = "sessions")
 public class Session {
@@ -16,7 +14,24 @@ public class Session {
     private String session_description;
     private Integer session_length;
 
+    private List<Speaker> speakers;
+
     public Session() {
+    }
+
+    @ManyToMany
+    @JoinTable(
+            name = "session_speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id")
+    )
+
+    public List<Speaker> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(List<Speaker> speakers) {
+        this.speakers = speakers;
     }
 
     public Long getSession_id() {
